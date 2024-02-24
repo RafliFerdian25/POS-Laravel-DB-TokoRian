ffdc02fc225c8d7457414702126c730664045f56
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-02-24 11:35:09 +0700

    `fix` create supplier

--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -116,7 +116,12 @@
             unhighlight: function(element, errorClass, validClass) {
                 $(element).removeClass('is-invalid');
             },
-            submitHandler: function(form) {
+            submitHandler: function(form, event) {
+                event.preventDefault();
+                $('#submitButton').html(
+                    '<svg class="spinners-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z"></path></svg>'
+                );
+                $('#submitButton').prop('disabled', true);
                 $.ajax({
                     type: "POST",
                     url: "{{ route('purchase.store') }}",
