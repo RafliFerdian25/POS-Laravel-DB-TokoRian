efcf1f2a0f00f9b8b2a18c005d771c934e582844
RafliFerdian25 <ferdianrafli25@gmail.com>
2024-05-08 09:37:32 +0700

    `feat` model product has expired before

---
+++ app/Http/Controllers/ProductHasExpiredBeforeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductHasExpiredBefore;
+use Illuminate\Http\Request;
+
+class ProductHasExpiredBeforeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductHasExpiredBefore  $productHasExpiredBefore
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductHasExpiredBefore $productHasExpiredBefore)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductHasExpiredBefore  $productHasExpiredBefore
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductHasExpiredBefore $productHasExpiredBefore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductHasExpiredBefore  $productHasExpiredBefore
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductHasExpiredBefore $productHasExpiredBefore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductHasExpiredBefore  $productHasExpiredBefore
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductHasExpiredBefore $productHasExpiredBefore)
+    {
+        //
+    }
+}
---
+++ app/Models/ProductHasExpiredBefore.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductHasExpiredBefore extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_barang_pernah_kadaluarsa';
+
+    protected $guarded = ['id'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_barang');
+    }
+}
